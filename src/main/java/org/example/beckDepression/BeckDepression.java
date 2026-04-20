7be09f7489220a4c9f6ef2e1b65bfa3fc0daddfe
anisiforov.deniska@gmail.com <anisiforov.deniska@gmail.com>
2026-04-20 19:11:58 +0700

    Добавлен метод answer. Тест зеленый.

--- src/main/java/org/example/beckDepression/BeckDepression.java
+++ src/main/java/org/example/beckDepression/BeckDepression.java
@@ -7,4 +7,8 @@ public class BeckDepression {
     public int getScore() {
         return this.score;
     }
+
+    public void answer(Question question, String answer) {
+        this.score += question.getScoreForAnswer(answer);
+    }
 }
