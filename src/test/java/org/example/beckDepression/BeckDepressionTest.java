3dc149942e8f5552f2330c9dfef7bf5737511751
anisiforov.deniska@gmail.com <anisiforov.deniska@gmail.com>
2026-04-20 19:11:05 +0700

    Добавлен тест для проверки успешности ответа на вопрос. Тест красный.

--- src/test/java/org/example/beckDepression/BeckDepressionTest.java
+++ src/test/java/org/example/beckDepression/BeckDepressionTest.java
@@ -2,6 +2,9 @@ package org.example.beckDepression;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,5 +22,18 @@ public class BeckDepressionTest {
         BeckDepression beck = new BeckDepression();
         int score = beck.getScore();
         assertEquals(0, score);
+    }
+
+    @Test
+    void shouldIncreaseScoreAfterAnswer() {
+        BeckDepression beck = new BeckDepression();
+        Map<String, Integer> input = new LinkedHashMap<>();
+        input.put("A", 0);
+        input.put("B", 1);
+        input.put("C", 2);
+        input.put("D", 3);
+        Question question = new Question(input);
+        beck.answer(question, "C");
+        assertEquals(2, beck.getScore());
     }
 }
