c907b52c7ecdb44dcd37e0835c5d9d9c8ac864a2
anisiforov.deniska@gmail.com <anisiforov.deniska@gmail.com>
2026-04-20 18:51:34 +0700

    Добавлен конструктор с параметрами и поле для класса Question. Тест зеленый.

---
+++ src/main/java/org/example/beckDepression/Question.java
@@ -0,0 +1,13 @@
+package org.example.beckDepression;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Question {
+
+    private final Map<String, Integer> answersAndScore;
+
+    public Question(Map<String, Integer> answersAndScore) {
+        this.answersAndScore = new LinkedHashMap<>(answersAndScore);
+    }
+}
