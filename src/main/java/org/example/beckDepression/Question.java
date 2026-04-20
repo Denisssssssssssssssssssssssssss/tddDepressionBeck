3e978645ff627ea0ce4869bcea628e6fd2b43e3f
anisiforov.deniska@gmail.com <anisiforov.deniska@gmail.com>
2026-04-20 19:00:38 +0700

    Исправлен метод getScoreForAnswer. Тест зеленый.

--- src/main/java/org/example/beckDepression/Question.java
+++ src/main/java/org/example/beckDepression/Question.java
@@ -12,6 +12,6 @@ public class Question {
     }
 
     public int getScoreForAnswer(String answer) {
-        return answersAndScore.get(answer);
+        return answersAndScore.getOrDefault(answer, -1);
     }
 }
